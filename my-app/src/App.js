67341a54dea7db28464f9c173d6a3490c961ede8
solipkimm@gmail.com <59662367+solipkimm@users.noreply.github.com>
2021-02-18 01:07:36 -0500

    delete logo.svg

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import { Button, Col, Container, Form, FormControl, Nav, Navbar, Row } from 'react-bootstrap';
 import { Redirect, Route, Switch } from 'react-router-dom';
@@ -6,24 +5,7 @@ import { LinkContainer } from 'react-router-bootstrap';
 
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return null
 }
 
 export default App;
