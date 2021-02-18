682c72873f137b1d2fc3697d0acaba82c7aacedf
solipkimm@gmail.com <59662367+solipkimm@users.noreply.github.com>
2021-02-18 01:33:17 -0500

    navbar

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -15,7 +15,31 @@ function App() {
     setSearchString("");
   }
 
-  return null
+  return (
+    <>
+      <Navbar bg="light" expand="lg">
+        <LinkContainer to="/">
+          <Navbar.Brand>New York Restaurants</Navbar.Brand>
+        </LinkContainer>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <LinkContainer to="/restaurants">
+              <Nav.Link>Full List</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/about">
+              <Nav.Link>About</Nav.Link>
+            </LinkContainer>
+          </Nav>
+          <Form onSubmit={handleSubmit} inline>
+            <FormControl type="text" placeholder="Borough" className="mr-sm-2" value={searchString} onChange={(e) => setSearchString(e.target.value)} />
+            <Button type="submit" variant="outline-success">Search</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Navbar>
+      <br />
+    </>
+  )
 }
 
 export default App;
