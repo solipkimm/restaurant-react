46e4f52a9e0bf599f82a31c879e8febe4092c6c5
solipkimm@gmail.com <59662367+solipkimm@users.noreply.github.com>
2021-02-19 02:02:16 -0500

    create card

--- my-app/src/About.js
+++ my-app/src/About.js
@@ -1,1 +1,19 @@
-<p>About</p>
+import { Card } from "react-bootstrap";
+
+export default function About() {
+  return (
+    <Card style={{ width: '30rem' }}>
+      <Card.Body>
+        <Card.Title> Solip Kim</Card.Title>
+        <p>
+          Student at Seneca College <br />
+          skim428@myseneca.ca
+        </p> 
+        <hr />
+        <Card.Text>
+          A lazy programmer
+        </Card.Text>
+      </Card.Body>
+    </Card>
+  );
+}
