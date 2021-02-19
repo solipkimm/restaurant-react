c3951f02a85cdbd7eb1861dab7f20e5e09792b2c
solipkimm@gmail.com <59662367+solipkimm@users.noreply.github.com>
2021-02-19 06:59:08 -0500

    add profile

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,7 +1,18 @@
+/*********************************************************************************
+*  WEB422 – Assignment 3
+*  I declare that this assignment is my own work in accordance with Seneca Academic Policy.  
+*  No part of this assignment has been copied manually or electronically from any other source
+*  (including web sites) or distributed to other students.
+* 
+*  Name: Solip Kim   Student ID: 120618194   Date: 19 Feb 2021
+*
+*
+********************************************************************************/ 
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './App';  
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter } from 'react-router-dom'
