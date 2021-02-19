a13ac1db475d63087e4db3e08e883b1f7e1ab61e
solipkimm@gmail.com <59662367+solipkimm@users.noreply.github.com>
2021-02-19 06:08:22 -0500

    update NotFound.js

---
+++ my-app/src/NotFound.js
@@ -0,0 +1,13 @@
+import { Card } from "react-bootstrap";
+
+function NotFound() {
+  return (
+    <Card>
+      <Card.Body>
+        <Card.Text>No Restaurants Found</Card.Text>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default NotFound;
